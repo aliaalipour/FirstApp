6d0117405848fa5aa5f340a2d2606fe7eb69265d
Aalipour <Aalipour@DESKTOP-95HK39E>
2018-12-20 16:43:21 +0330

    First Change

---
+++ FirstApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 1&2
+            //Har barname ba click Ctrl+F5 Ejra mishavad
+            //baraye namayesh
+            Console.WriteLine("Hello World!!!");
+            #endregion
+
+            #region 3
+            //int,string,double,decimal
+
+            //int num1;
+            //num1 = 10000;
+
+            //string name = "ali aalipoue";
+
+            //Console.WriteLine(num1);
+            //Console.WriteLine(name);
+
+            #endregion
+
+            #region 4
+
+            //int num1, num2;
+            //num1 = 10;
+            //num2 = 2;
+
+            //int sum = num1 + num2;
+            //int min = num1 - num2;
+            //int div = num1 / num2;
+            //int mul = num1 * num2;
+
+            //Console.WriteLine("The Sum is: "+ sum);
+            //Console.WriteLine("The minus is: "+ min);
+            //Console.WriteLine("The Divided is: "+ div);
+            //Console.WriteLine("The mul is: " + mul);
+
+
+
+            #endregion
+
+            #region 5
+
+
+            //int num1, num2;
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //int sum = num1 + num2;
+            //int min = num1 - num2;
+            //int div = num1 / num2;
+            //int mul = num1 * num2;
+
+            //Console.WriteLine("The Sum is: " + sum);
+            //Console.WriteLine("The minus is: " + min);
+            //Console.WriteLine("The Divided is: " + div);
+            //Console.WriteLine("The mul is: " + mul);
+
+            #endregion
+
+        }
+    }
+}
